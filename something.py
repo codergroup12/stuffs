be1306252f9f8b57a289ae735a481f30269aed6a
avneh <70001510+avneh@users.noreply.github.com>
2021-01-27 11:05:14 -0800

    Update something.py

--- something.py
+++ something.py
@@ -1,7 +1,6 @@
-def printTimesTables():
-  for x in range(1,12):
-    for y in range(1,12):
-      print(x + " times " + y + " is " + x*y)
-    print("")
-
-printTimesTables()
+def TimesTables():
+    for x in range(1,12):
+        for y in range(1,12):
+            print(str(x) + " times " + str(y) + " is " + str(x*y))
+        print("")
+TimesTables()
